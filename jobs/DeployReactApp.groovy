1219bc1d9af9fbfc3c01e923bbb090f95989cedd
Serdar <gulec2000@gmail.com>
2021-03-27 05:28:14 +0100

    edited jobs/DeployReactApp.groovy

--- jobs/DeployReactApp.groovy
+++ jobs/DeployReactApp.groovy
@@ -28,28 +28,3 @@ pipelineJob('Deploy-React-App'){
     }
   }
 }
-/*pipelineJob('Destroy-React-App'){
-/*    description('Destroys a React web application to AWS')
-/*    logRotator {
-/*        daysToKeep(5)
-/*        numToKeep(20)
-/*    }
-/*    concurrentBuild(allowConcurrentBuild = false)
-/*    parameters {
-/*      stringParam('UNIQUE_ANIMAL_IDENTIFIER', defaultValue = 'spider', description = 'Your unique animal identifier for this playground!')
-/*    }
-/*    definition {
-/*    cpsScm {
-/*      scm {
-/*        git {
-/*          branch('master')
-/*          remote {
-/*            credentials('${GIT_USER}')
-/*            url('${GIT_URL}')
-/*          }
-/*        }
-/*      }
-/*      scriptPath('destroy.Jenkinsfile')
-/*    }
-/*  }
-/*}
