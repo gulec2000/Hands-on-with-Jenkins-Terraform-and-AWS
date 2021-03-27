2c9e29f2c9d06a643fd31a4a62326c5e8ac2885c
Serdar <gulec2000@gmail.com>
2021-03-28 00:00:26 +0100

    edited terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ module "dpg_novmeber_elb" {
 module "dpg_november_asg" {
   source                   = "./modules/autoscaling_group"
   security_group_id        = data.aws_security_group.sg.id
-  subnet_id                = data.aws_subnet.sn.id
+  subnet_id                = var.subnet_id
   elb_id                   = module.dpg_novmeber_elb.elb_id
   UNIQUE_ANIMAL_IDENTIFIER = var.UNIQUE_ANIMAL_IDENTIFIER
 }
@@ -64,8 +64,4 @@ resource "aws_subnet" "sn" {
 
 data "aws_security_group" "sg" {
   id = var.security_group_id
-}
-
-data "aws_subnet" "sn" {
-  id = var.subnet_id
 }
