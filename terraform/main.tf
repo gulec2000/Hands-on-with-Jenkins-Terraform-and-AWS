f233badb52fe29287babde66cee037a6a90186c0
Serdar <gulec2000@gmail.com>
2021-03-27 18:45:58 +0100

    edited terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,20 @@ module "dpg_november_asg" {
   elb_id                   = module.dpg_novmeber_elb.elb_id
   UNIQUE_ANIMAL_IDENTIFIER = var.UNIQUE_ANIMAL_IDENTIFIER
 }
+data "aws_security_group" "sg" {
+  tags = {
+    "Purpose" = "Playground"
+  }
+}
+data "aws_subnet" "sn"{
+  vpc_id = data.aws_vpc.vpc.id
+  tags = {
+    Purpose = "Playground"
+    count = 0
+  }
+}
+data "aws_vpc" "vpc"{
+  tags = {
+    Purpose = "Playground"
+  }
+}
