3e965d783015aa6a311b19127cb7986b2eb72770
Serdar <gulec2000@gmail.com>
2021-03-28 22:10:41 +0200

    deletion of infrasructure

--- jobs/DeployReactApp.groovy
+++ jobs/DeployReactApp.groovy
@@ -23,34 +23,3 @@ pipelineJob("Destroy-React-App"){
     }
   }
 }
-/*pipelineJob('Deploy-React-App'){
-/*    description('Deploys a React web application to AWS')
-/*    logRotator {
-/*        daysToKeep(5)
-/*        numToKeep(20)
-/*    }
-/*    concurrentBuild(allowConcurrentBuild = false)
-/*    triggers {
-/*        scm('* * * * *'){
-/*            ignorePostCommitHooks(ignorePostCommitHooks = false)
-/*        }
-/*    }
-/*    parameters {
-/*      stringParam('UNIQUE_ANIMAL_IDENTIFIER', defaultValue = 'spider', description = 'Your unique animal identifier for this playground!')
-/*    }
-/*    definition {
-/*    cpsScm {
-/*      scm {
-/*        git {
-/*          branch('master')
-/*          remote {
-/*            credentials('${GIT_USER}')
-/*            url('${GIT_URL}')
-/*          }
-/*        }
-/*      }
-/*      scriptPath('Jenkinsfile')
-/*    }
-/*  }
-/*}
-/*
