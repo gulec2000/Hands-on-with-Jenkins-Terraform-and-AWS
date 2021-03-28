66cecbdbd7d6fec3fd718476d4cbe73ce5e3b6a0
Serdar <gulec2000@gmail.com>
2021-03-28 22:06:13 +0200

    deletion of infrasructure

--- jobs/DeployReactApp.groovy
+++ jobs/DeployReactApp.groovy
@@ -1,30 +1,56 @@
-pipelineJob('Deploy-React-App'){
-    description('Deploys a React web application to AWS')
+pipelineJob("Destroy-React-App"){
+    description("Destroys a React web application to AWS")
     logRotator {
         daysToKeep(5)
         numToKeep(20)
     }
     concurrentBuild(allowConcurrentBuild = false)
-    triggers {
-        scm('* * * * *'){
-            ignorePostCommitHooks(ignorePostCommitHooks = false)
-        }
-    }
     parameters {
-      stringParam('UNIQUE_ANIMAL_IDENTIFIER', defaultValue = 'spider', description = 'Your unique animal identifier for this playground!')
+      stringParam("UNIQUE_ANIMAL_IDENTIFIER", defaultValue = "changeme", description = "Your unique animal identifier for this playground!")
     }
     definition {
     cpsScm {
       scm {
         git {
-          branch('master')
+          branch("master")
           remote {
-            credentials('${GIT_USER}')
-            url('${GIT_URL}')
+            credentials("${GIT_USER}")
+            url("${GIT_URL}")
           }
         }
       }
-      scriptPath('Jenkinsfile')
+      scriptPath('destroy.Jenkinsfile')
     }
   }
 }
+/*pipelineJob('Deploy-React-App'){
+/*    description('Deploys a React web application to AWS')
+/*    logRotator {
+/*        daysToKeep(5)
+/*        numToKeep(20)
+/*    }
+/*    concurrentBuild(allowConcurrentBuild = false)
+/*    triggers {
+/*        scm('* * * * *'){
+/*            ignorePostCommitHooks(ignorePostCommitHooks = false)
+/*        }
+/*    }
+/*    parameters {
+/*      stringParam('UNIQUE_ANIMAL_IDENTIFIER', defaultValue = 'spider', description = 'Your unique animal identifier for this playground!')
+/*    }
+/*    definition {
+/*    cpsScm {
+/*      scm {
+/*        git {
+/*          branch('master')
+/*          remote {
+/*            credentials('${GIT_USER}')
+/*            url('${GIT_URL}')
+/*          }
+/*        }
+/*      }
+/*      scriptPath('Jenkinsfile')
+/*    }
+/*  }
+/*}
+/*
