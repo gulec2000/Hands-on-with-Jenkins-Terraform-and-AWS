53acd68de2e580303f053980b36d8f38979a6fbf
Serdar <gulec2000@gmail.com>
2021-03-28 21:48:37 +0200

    edited

--- terraform/modules/elastic_load_balancer/main.tf
+++ terraform/modules/elastic_load_balancer/main.tf
@@ -1,49 +1,29 @@
-# Create the Auto Scaling launch config
-data "template_file" "init" {
-  template = file("scripts/deploy-react-application.sh.tpl")
+# Create the ELB
+resource "aws_elb" "elb" {
+  name            = "playground-${var.UNIQUE_ANIMAL_IDENTIFIER}"
+  security_groups = [var.security_group_id]
+  subnets         = [var.subnet_id]
 
-  vars = {
-    UNIQUE_ANIMAL_IDENTIFIER = "${var.UNIQUE_ANIMAL_IDENTIFIER}"
-  }
-}
-
-resource "aws_launch_configuration" "lc" {
-  name_prefix                 = "playground-"
-  image_id                    = data.aws_ami.amazon-linux-2.id
-  instance_type               = "t2.small"
-  security_groups             = [var.security_group_id]
-  user_data                   = data.template_file.init.rendered
-  key_name                    = "playground-november-key"
-  associate_public_ip_address = true
-  iam_instance_profile        = aws_iam_instance_profile.deploy_profile.name
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-# Create the Auto Scaling group
-resource "aws_autoscaling_group" "asg" {
-  name                 = "playground-${aws_launch_configuration.lc.name}-dpg-november"
-  max_size             = 3
-  min_size             = 1
-  desired_capacity     = 1
-  health_check_type    = "EC2"
-  launch_configuration = aws_launch_configuration.lc.name
-  vpc_zone_identifier  = [var.subnet_id]
-  load_balancers       = [var.elb_id]
-
-  lifecycle {
-    create_before_destroy = true
+  listener {
+    instance_port     = 80
+    instance_protocol = "tcp"
+    lb_port           = 80
+    lb_protocol       = "tcp"
   }
 
-  tag {
-    key                 = "Name"
-    value               = lower("playground-${var.UNIQUE_ANIMAL_IDENTIFIER}-dpg-november")
-    propagate_at_launch = true
+  health_check {
+    healthy_threshold   = 10
+    unhealthy_threshold = 2
+    timeout             = 5
+    target              = "TCP:80"
+    interval            = 30
   }
 
-  timeouts {
-    delete = "15m"
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "playground-${var.UNIQUE_ANIMAL_IDENTIFIER}"
   }
 }
