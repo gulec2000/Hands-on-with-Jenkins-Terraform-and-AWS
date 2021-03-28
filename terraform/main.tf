83997e98af1ba78c332f8c7bf1f86653af6930d2
Serdar <gulec2000@gmail.com>
2021-03-28 01:08:09 +0100

    edited terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,13 +13,16 @@ module "dpg_november_asg" {
   UNIQUE_ANIMAL_IDENTIFIER = var.UNIQUE_ANIMAL_IDENTIFIER
 }
 resource "aws_vpc" "vpc1" {
-  cidr_block           = "172.16.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = "true"
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
 resource "aws_subnet" "subnet1" {
   availability_zone       = "eu-west-2a"
   vpc_id                  = aws_vpc.vpc1.id
-  cidr_block              = "172.16.0.0/24"
+  cidr_block              = "10.0.101.0/24"
   map_public_ip_on_launch = "true"
 
 }
