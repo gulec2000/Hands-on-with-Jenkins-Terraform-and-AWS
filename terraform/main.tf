cef9c4f5039c86ced884a942e00b876f48796371
Serdar <gulec2000@gmail.com>
2021-03-28 01:14:16 +0100

    edited terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,40 @@ module "dpg_november_asg" {
   elb_id                   = module.dpg_novmeber_elb.elb_id
   UNIQUE_ANIMAL_IDENTIFIER = var.UNIQUE_ANIMAL_IDENTIFIER
 }
+
+resource "aws_security_group" "sg" {
+  name        = "sg"
+  vpc_id      = aws_vpc.vpc1.id
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_vpc" "vpc1" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = "true"
@@ -45,37 +79,4 @@ resource "aws_route_table" "route_table1" {
 resource "aws_route_table_association" "route-subnet1" {
   subnet_id = aws_subnet.subnet1.id
   route_table_id = aws_route_table.route_table1.id
-}
-
-resource "aws_security_group" "sg" {
-  name        = "sg"
-  vpc_id      = aws_vpc.vpc1.id
-
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
